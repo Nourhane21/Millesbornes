333786d934c985e82910d945240926eaa680fa1d
Nova <nourhane.mallek@univ-tlse3.fr>
2025-03-19 11:37:18 +0100

    TP2

--- src/cartes/Carte.java
+++ src/cartes/Carte.java
@@ -1,5 +1,18 @@
 package cartes;
 
 public abstract class Carte {
-
+	 public Carte() {
+	    }
+	@Override
+    public boolean equals(Object obj) {
+        if (obj==null){
+            return false;
+        }
+        if(obj.getClass()==this.getClass()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
